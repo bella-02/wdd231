2f4523f38ae6c2ecbd2e581e341384b376e40ad1
bella-02 <bellaevehunt@gmail.com>
2024-11-16 12:40:41 -0700

    chamber directory page created

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,68 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const membersContainer = document.getElementById('members');
+    const gridViewButton = document.getElementById('grid-view-button');
+    const listViewButton = document.getElementById('list-view-button');
+    const lastModifiedElement = document.getElementById("last-modified");
+
+    async function fetchMembers() {
+
+            const response = await fetch('./data/members.json');
+            const members = await response.json();
+            displayMembers(members, 'grid');
+ 
+    }
+
+    function displayMembers(members, viewType) {
+        membersContainer.innerHTML = '';
+        membersContainer.className = viewType;
+
+        members.forEach(member => {
+            const memberCard = document.createElement('div');
+            memberCard.classList.add('member-card');
+
+            memberCard.innerHTML = `
+                <img src="${member.image}" alt="${member.name}">
+                <p>${member.name}</p>
+                <p>${member.address}</p>
+                <p>${member.phone}</p>
+                <p><a href="${member.website}" target="_blank">${member.website}</a></p>
+            `;
+
+            membersContainer.appendChild(memberCard);
+        });
+    }
+
+    function getMembershipLevel(level) {
+        switch (level) {
+            case 1: return 'Member';
+            case 2: return 'Silver';
+            case 3: return 'Gold';
+            default: return 'Unknown';
+        }
+    }
+
+   
+    gridViewButton.addEventListener('click', () => {
+        membersContainer.classList.remove('list');
+        membersContainer.classList.add('grid');
+    });
+
+    
+    listViewButton.addEventListener('click', () => {
+        membersContainer.classList.remove('grid');
+        membersContainer.classList.add('list');
+    });
+
+    
+    fetchMembers();
+
+    lastModifiedElement.textContent = `Last Update: ${document.lastModified}`;
+});
+
+document.querySelector(".menu-button").addEventListener("click", () => {
+    document.querySelector("nav").classList.toggle("active");
+});
+
+
+
